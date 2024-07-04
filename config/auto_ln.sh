a66ee21108c186bf5845266ee3c2878905d722c4
jianjia <1208872131@qq.com>
2024-07-04 22:50:38 +0800

    fix auto_ln.sh

--- config/auto_ln.sh
+++ config/auto_ln.sh
@@ -2,17 +2,12 @@
 
 dir=$HOME/dotfile
 
-# bash
-f_bash=$dir/bash
-echo "ln -si $f_bash/.* $HOME/"
-ln -si $f_bash/.* $HOME/
-
 # zsh (oh-my-zsh, powerlevel10k, highlight, suggestion needed)
-f_zsh=$dir/zsh
-echo "ln -si $f_zsh/.* $HOME/"
-ln -si $f_zsh/.* $HOME/
+home=$dir/home
+echo "ln -si $home/.* $HOME/"
+ln -si $home/.* $HOME/
 
 # other config in ~/.config
-f_config=$dir/config
-echo "ln -si $f_config/* $HOME/.config/"
-ln -si $f_config/* $HOME/.config/
+config=$dir/config
+echo "ln -si $config/* $HOME/.config/"
+ln -si $config/* $HOME/.config/
