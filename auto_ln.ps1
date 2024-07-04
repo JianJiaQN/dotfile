1282a4491b3cd370dbd543e55647fcff7991e07e
jianjia <1208872131@qq.com>
2024-07-04 23:49:26 +0800

    win11 config dotfile, init

---
+++ auto_ln.ps1
@@ -0,0 +1,8 @@
+cmd /c mklink /D 'C:\Users\jianjia\.config' D:\JianJia\dotfile\config
+
+cmd /c mklink 'C:\Users\jianjia\.gitconfig' D:\JianJia\dotfile\home\.gitconfig
+
+cmd /c mklink 'C:\Users\jianjia\.mambarc' D:\JianJia\dotfile\home\.mambarc
+
+cmd /c mklink 'C:\Users\jianjia\.wslconfig' D:\JianJia\dotfile\home\.wslconfig
+
